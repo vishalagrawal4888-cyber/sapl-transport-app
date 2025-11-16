1912927afa2050eda04858c7a737f87df30559ea
vishalagrawal4888-cyber <vishalagrawal4888@gmail.com>
2025-11-16 16:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+# Home page
+@app.route('/')
 def home():
-    return "Sapl New Project Transport App is Running Successfully!"
+    return render_template('home.html')
+
+# Login Page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == "admin" and password == "1234":
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid login!"
+
+    return render_template('login.html')
+
+# Register Page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return "Registration successful!"
+    return render_template('register.html')
+
+# Dashboard Page
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == "__main__":
     app.run()
